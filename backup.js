ae38a711fd26b693df50c6b9956079a6105ddbd8
andrezuu <sanchezandres8203@gmail.com>
2025-05-31 22:28:05 -0400

    fix: backups

--- backup.js
+++ backup.js
@@ -1,6 +1,7 @@
 const cron = require("cron");
 const { exec } = require("child_process");
 const dotenv = require("dotenv");
+const dayjs = require("dayjs");
 
 dotenv.config();
 const job = new cron.CronJob(
@@ -13,7 +14,7 @@ const job = new cron.CronJob(
     const database = "apuestas_db";
     const folder = "/tmp";
     const currentDate = new Date();
-    const fileName = `backup_${currentDate.toISOString()}.dump`;
+    const fileName = `backup_${dayjs().format("YYYYMMDD_HHmmss")}.dump`;
     const backupCommand = `docker exec -u ${dockerUser} ${dockerContainer} \
          pg_dump -U ${user} -F c -d ${database} -f ${folder}/${fileName}`;
     const copyCommand = `docker cp ${dockerContainer}:/tmp/${fileName} ./backups/${fileName}`;
